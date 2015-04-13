07a3e704cf449fa926dcd0652b765480a092f069
whmeitzler <whmeitzler@olivet.edu>
2015-04-12 19:15:22 -0500

    Commit...

--- myo/src/main/java/edu/olivet/myo/Main.java
+++ myo/src/main/java/edu/olivet/myo/Main.java
@@ -1,14 +1,21 @@
 package edu.olivet.myo;
 
+import javax.swing.JOptionPane;
+
 import org.thingml.bglib.*;
 import org.thingml.bglib.gui.*;
 
 public class Main {
-	public static void main(String[] args) throws Throwable {
+	public static void main(String[] args){
+		BLEExplorerFrame f = new BLEExplorerFrame();
+		f.main(args);
+	}
+	public static void amain(String[] args) throws Throwable {
 		//BLED112.getAvailableSerialPorts().stream().map(e -> e.getName()).forEach(System.out::println);
-		BGAPITransport trans = BLED112.connectBLED112("COM5");//("/dev/tty.usbmodem1");
+		BGAPITransport trans = BLED112.connectBLED112(BLED112.selectSerialPort());//("/dev/tty.usbmodem1");
 		try {
 			BGAPI api = new BGAPI(trans);
+			BGAPIPacketLogger logger = new BGAPIPacketLogger();
 			api.addListener(new BGAPIDefaultListener() {
 			    @SuppressWarnings("unused")
 				public void receive_system_get_info(Integer major, Integer minor, Integer patch, Integer build, Integer ll_version, Integer protocol_version, Integer hw) {
@@ -17,7 +24,8 @@ public class Main {
 
 			    @SuppressWarnings("unused")
 				public void receive_gap_scan_response(Integer rssi, Integer packet_type, BDAddr sender, Integer address_type, Integer bond, byte[] data) {
-			        System.out.println("FOUND: " + sender.toString() + "["+ new String(data).trim() + "] (rssi = " + rssi + ", packet type= " + packet_type + ")");
+			        JOptionPane.showMessageDialog(null,null,  "FOUND: " + sender.toString() + "["+ new String(data).trim() + "] (rssi = " + rssi + ", packet type= " + packet_type + ")", JOptionPane.INFORMATION_MESSAGE);
+			    	System.err.println("FOUND: " + sender.toString() + "["+ new String(data).trim() + "] (rssi = " + rssi + ", packet type= " + packet_type + ")");
 			    }
 
 			    @Override
@@ -60,14 +68,15 @@ public class Main {
 	        } catch (InterruptedException ex) { }
 	        System.out.println("Requesting Version Number...");
 			api.send_system_get_info();
+			Thread.sleep(2000);
 			api.send_system_hello();
+			Thread.sleep(3000);
+			api.getLowLevelDriver().addListener(logger);
 			api.send_gap_set_scan_parameters(10, 250, 1);
-			api.send_gap_discover(1);
-	        try {
-	            Thread.sleep(50000);
-	        } catch (InterruptedException ex) { }
-		} finally {
-			trans.stop();
-		}
+			Thread.sleep(100);
+			System.out.println("Scanning");
+	        	api.send_gap_discover(1);
+	            Thread.sleep(5000);
+		} finally {}
 	}
 }
